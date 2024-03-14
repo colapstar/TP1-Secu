0c4721f5317c3c67b116611b7dd5d04ca4303470
Moran <moran38villard@gmail.com>
2024-03-14 14:08:38 +0100

    letters + letters

--- Knapsack_TP1_source/TP1.c
+++ Knapsack_TP1_source/TP1.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
+char chiffrement_vigenere(char lettre_texte, char lettre_cle) {
+    // Décalage correspondant à la lettre de la clé
+    int decalage = lettre_cle - 'a';
+    
+    // Calcul de la lettre chiffrée
+    char lettre_chiffree = ((lettre_texte - 'a' + decalage) % 26) + 'a';
+    
+    return lettre_chiffree;
+}
+
 int main() {
-    printf("Hello, World! tedsqffq<\n");
+    char lettre_texte, lettre_cle;
+    
+    printf("Entrez une lettre du texte (minuscule) : ");
+    scanf("%c", &lettre_texte);
+    getchar(); // pour consommer le caractère de nouvelle ligne restant
+    
+    printf("Entrez une lettre de la clé (minuscule) : ");
+    scanf("%c", &lettre_cle);
+    getchar(); // pour consommer le caractère de nouvelle ligne restant
+    
+    char lettre_chiffree = chiffrement_vigenere(lettre_texte, lettre_cle);
+    
+    printf("%c = %d\n", lettre_cle, lettre_cle - 'a');
+    printf("%c + %d = %c\n", lettre_texte, lettre_cle - 'a', lettre_chiffree);
+    
     return 0;
 }
